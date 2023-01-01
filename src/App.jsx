95218a86b4778caf30919e352a8d25ad960c6796
Got-crypto <pecpalmplay@gmail.com>
2023-01-01 04:34:58 +0200

    designed a CreateCampaign page

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,9 @@ export default function App(){
 
                 <Routes>
                     <Route path='/' element={<Home />} />
+                    <Route path='/profile' element={<Profile/>} />
+                    <Route path='/campaign-details/:id' element={<CampaignDetails />}  />
+                    <Route path='/create-campaign' element={<CreateCampaign/>} />
                 </Routes>
             </div>
         </div>
